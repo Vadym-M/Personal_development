c595f1d91f8c4f1211b972404f78dc1beb324019
Vadym-Muntianov <59168867+Vadym-Muntianov@users.noreply.github.com>
2021-02-01 22:13:27 +0200

    connect Firebase
    add dependencies:
    firebase database
    firebase storage
    firebase analytics

--- app/src/main/java/com/vinade_app/personaldevelopment/Card.java
+++ app/src/main/java/com/vinade_app/personaldevelopment/Card.java
@@ -3,5 +3,36 @@ package com.vinade_app.personaldevelopment;
 import java.util.ArrayList;
 
 public class Card {
-    
+    ArrayList<String>imageName;
+    String idCard, text;
+
+    public Card(ArrayList<String> imageName, String idCard, String text) {
+        this.imageName = imageName;
+        this.idCard = idCard;
+        this.text = text;
+    }
+
+    public ArrayList<String> getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(ArrayList<String> imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- app/src/main/java/com/vinade_app/personaldevelopment/MainActivity.java
+++ app/src/main/java/com/vinade_app/personaldevelopment/MainActivity.java
@@ -1,18 +1,32 @@
 package com.vinade_app.personaldevelopment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    ImageView mImageView;
     ArrayList<Image> testList =  new ArrayList<Image>();
     int[] images = {R.drawable.b,  R.drawable.a, R.drawable.c, R.drawable.index};
 
@@ -20,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.imageView2);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("image1");
+        
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference rootRef = storage.getReference().child("test1.jpg");
+        try{
+            File localFile = File.createTempFile("images", "jpg");
+        rootRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                mImageView.setImageBitmap(bitmap);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+            }
+        });
+    } catch (IOException e ) {}
         Log.d("debug","Main act" + images[0]);
        
     Init();
